a599717250c93c22739b6a68f5aa28ba06ea786c
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-28 10:38:53 -0300

    feat: Implementando tela de 404

---
+++ src/pages/_error.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NotFound() {
+  const navigate = useRouter();
+
+  const goBack = () => {
+    navigate.back();
+  };
+
+  return (
+    <div className="flex flex-col w-screen h-screen justify-center items-center select-none gap-4">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="/gif/404.gif" alt="404 Not Found" />
+
+      <div className="text-center">
+        <h1 className="font-bold text-3xl my-3">
+          Ops... Parece que você se perdeu!
+        </h1>
+        <span className="font-semibold text-gray-500 text-lg">
+          Clique em Voltar para retornar à página anterior.
+        </span>
+      </div>
+      <button
+        onClick={goBack}
+        className="rounded-md py-2 px-4 bg-red-500 text-white w-80 text-lg font-semibold"
+      >
+        Voltar
+      </button>
+    </div>
+  );
+}
