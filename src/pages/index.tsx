4b06cb81015efd20538e8e5abf5fd24d8cf3a096
AlehandroSV <alehandrosantos99@gmail.com>
2023-07-02 11:57:40 -0300

    feat: Implementando cards e inicializando tela de Atendimentos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,80 +1,161 @@
+import { Card } from "@/shared/components/Card";
 import Result from "@/shared/components/Result";
 import api from "@/shared/services";
-import {GetServerSideProps} from "next";
-import {parseCookies} from "nookies";
-import {useState} from "react";
+import { User } from "@phosphor-icons/react";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { parseCookies } from "nookies";
+import { useState } from "react";
 
 export default function Dashboard() {
-    const [result, setResult] = useState({text: "Teste", status: false});
-    const [isOpenResult, setIsOpenResult] = useState<boolean>(false);
+  const [result, setResult] = useState({ text: "Teste", status: false });
+  const [isOpenResult, setIsOpenResult] = useState<boolean>(false);
 
-    const vala = () => {
-        const url = "https://api.imgbb.com/1/upload";
-        const apiKey = process.env.TOKEN_API_IMG;
-        const imageUrl = "https://picsum.photos/200/300";
+  const cardInfos = [
+    {
+      id: "1",
+      title: "Teste",
+      description: "Teste",
+      icon: <User size={24} />,
+      value: "12",
+    },
+    {
+      id: "2",
+      title: "Teste",
+      description: "Teste",
+      icon: <User size={24} />,
+      value: "12",
+    },
+    {
+      id: "3",
+      title: "Teste",
+      description: "Teste",
+      icon: <User size={24} />,
+      value: "12",
+    },
+    {
+      id: "4",
+      title: "Teste",
+      description: "Teste",
+      icon: <User size={24} />,
+      value: "12",
+    },
+    {
+      id: "5",
+      title: "Teste",
+      description: "Teste",
+      icon: <User size={24} />,
+      value: "12",
+    },
+    {
+      id: "6",
+      title: "Teste",
+      description: "Teste",
+      icon: <User size={24} />,
+      value: "12",
+    },
+  ];
 
-        const config = {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            params: {
-                expiration: 600,
-                key: apiKey,
-            },
-        };
+  const teste = () => {
+    const url = "https://api.imgbb.com/1/upload";
+    const apiKey = process.env.TOKEN_API_IMG;
+    const imageUrl = "https://picsum.photos/200/300";
 
-        const formData = new URLSearchParams();
-        formData.append("image", imageUrl);
-
-        api()
-            .post(url, formData.toString(), config)
-            .then((response) => {
-                console.log(response.data);
-                setResult({text: "Deu bom!", status: true});
-            })
-            .catch((error) => {
-                console.error(error);
-                setResult({text: "Deu erro!", status: false});
-            })
-            .finally(toggleResult);
+    const config = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      params: {
+        expiration: 600,
+        key: apiKey,
+      },
     };
 
-    const toggleResult = () => {
-        setIsOpenResult(!isOpenResult);
-    };
-    return (
-        <>
-            <h1 onClick={vala}> Home</h1>
+    const formData = new URLSearchParams();
+    formData.append("image", imageUrl);
 
-            <Result
-                text={result.text}
-                status={result.status}
-                open={isOpenResult}
-                onClose={toggleResult}
-            />
-        </>
-    );
+    api()
+      .post(url, formData.toString(), config)
+      .then((response) => {
+        console.log(response.data);
+        setResult({ text: "Deu bom!", status: true });
+      })
+      .catch((error) => {
+        console.error(error);
+        setResult({ text: "Deu erro!", status: false });
+      })
+      .finally(toggleResult);
+  };
+
+  const toggleResult = () => {
+    setIsOpenResult(!isOpenResult);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Dashboard | Atendimentos</title>
+      </Head>
+
+      <div className="flex gap-2 flex-wrap mx-4 my-2 justify-center">
+        {cardInfos.map((cardInfo) => (
+          <Card
+            key={cardInfo.id}
+            title={cardInfo.title}
+            icon={cardInfo.icon}
+            value={cardInfo.value}
+          />
+        ))}
+      </div>
+
+      <div className="my-4 mx-8 w-screen">
+        <table className="w-full">
+          <thead className="text-left">
+            <tr>
+              <th>Descrição</th>
+              <th>Valor</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Teste</td>
+              <td>12</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <Result
+        text={result.text}
+        status={result.status}
+        open={isOpenResult}
+        onClose={toggleResult}
+      />
+    </>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    const {["BearerToken"]: token} = parseCookies(ctx);
+  const { ["BearerToken"]: token } = parseCookies(ctx);
 
-    if (!token) {
-        return {
-            redirect: {
-                destination: "/user/login",
-                permanent: false,
-            },
-        };
-    }
-
-    let isLogin = false;
-
-    token ? (isLogin = true) : isLogin;
-
+  if (!token) {
     return {
-        props: {
-            isLogin,
-        },
+      redirect: {
+        destination: "/user/login",
+        permanent: false,
+      },
     };
+  }
+
+  let isLogin = false;
+
+  token ? (isLogin = true) : isLogin;
+
+  return {
+    props: {
+      isLogin,
+    },
+  };
 };
---
+++ src/shared/components/Card.tsx
@@ -0,0 +1,18 @@
+interface CardProps {
+  title: string;
+  icon: React.ReactNode;
+  value: string;
+}
+
+export const Card = ({ title, icon, value }: CardProps) => {
+  return (
+    <div className="w-64 flex flex-col gap-4 shadow-md rounded-md p-4">
+      <h6 className="text-lg">{title}</h6>
+
+      <div className="flex items-center justify-between">
+        {icon}
+        <span className="text-2xl">{value}</span>
+      </div>
+    </div>
+  );
+};
