331e6e2a0f25ccdd6cf4222b4c17ade094aa3e01
AlehandroSV <alehandrosantos99@gmail.com>
2023-07-11 08:26:08 -0300

    Tendo resolver Deploy Vercel

--- next.config.js
+++ next.config.js
@@ -4,10 +4,15 @@ dotenv.config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    env: {
-        TOKEN_API_IMG: process.env.TOKEN_API_IMG,
+  reactStrictMode: true,
+  env: {
+    TOKEN_API_IMG: process.env.TOKEN_API_IMG,
+  },
+  modularizeImports: {
+    "@phosphor-icons/react": {
+      transform: "@phosphor-icons/react/{{member}}",
     },
+  },
 };
 
 module.exports = nextConfig;
--- src/shared/routes/index.tsx
+++ src/shared/routes/index.tsx
@@ -1,18 +1,25 @@
-import {Buildings, ChartPieSlice, Gauge, RoadHorizon, TrafficSign, User,} from "@phosphor-icons/react";
-import {IRoutes} from "../interfaces/RoutesData";
+import {
+  Buildings,
+  ChartPieSlice,
+  Gauge,
+  RoadHorizon,
+  TrafficSign,
+  User,
+} from "@phosphor-icons/react";
+import { IRoutes } from "../interfaces/RoutesData";
 
 export const RoutesProps: IRoutes[] = [
-  {title: "Dashboard", href: "/", icon: <Gauge size={32}/>},
-  {title: "Setor", href: "sector", icon: <Buildings size={32}/>},
-  {title: "Funcionários", href: "user", icon: <User size={32}/>},
+  { title: "Bairro", href: "neighborhood", icon: <RoadHorizon size={32} /> },
+  { title: "Dashboard", href: "/", icon: <Gauge size={32} /> },
+  { title: "Funcionários", href: "user", icon: <User size={32} /> },
+  { title: "Setor", href: "sector", icon: <Buildings size={32} /> },
   {
     title: "Relatórios",
-    icon: <ChartPieSlice size={32}/>,
+    icon: <ChartPieSlice size={32} />,
     children: [
-      {title: "Teste", href: "report", icon: <ChartPieSlice size={28}/>},
+      { title: "Teste", href: "report", icon: <ChartPieSlice size={28} /> },
     ],
     open: false,
   },
-  {title: "Rua", href: "road", icon: <TrafficSign size={32}/>},
-  {title: "Bairro", href: "neighborhood", icon: <RoadHorizon size={32}/>},
+  { title: "Rua", href: "road", icon: <TrafficSign size={32} /> },
 ];
